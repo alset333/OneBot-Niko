6563fda3dcbaf3fd13daa5d40d890151b1c8648a
Peter Maar <alset333@icloud.com>
2019-05-21 17:02:40 -0400

    Save translations for speed and less API usage

--- WatsonTranslatorV3Utility.py
+++ WatsonTranslatorV3Utility.py
@@ -23,12 +23,43 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 from watson_developer_cloud import LanguageTranslatorV3
 from watson_developer_cloud.watson_service import WatsonApiException
 import re
+import os
+import json
 
 # 0 Dallas, 1 Washington DC, 2 Frankfurt, 3 Sydney, 4 Tokyo, 5 London
 LOCATION_NUMBER = 1
 
 url_locations = ["", "-wdc", "-fra", "-syd", "-tok", "-lon"]
 url_location = url_locations[LOCATION_NUMBER]
+
+# Detections contains {phrase: lang}
+# Translations contains {input_lang: {output_lang: {input_phrase: output_phrase}}}
+saved_translations = {'detections': {}, 'translations': {}}
+saved_translations_path = os.path.normpath('saved_translations.json')
+
+
+def load_translations():
+    global saved_translations
+
+    if os.path.isfile(saved_translations_path):
+        saved_translations_file = open(saved_translations_path)
+
+        translation_file_contents = saved_translations_file.read()  # Read the contents
+
+        if len(str.rstrip(translation_file_contents)) != 0:
+            saved_translations = json.loads(translation_file_contents)  # Read and decode the JSON from the file
+            print(saved_translations)
+
+        saved_translations_file.close()
+
+
+def save_translations():
+    print("Saving...")
+    saved_translations_file = open(saved_translations_path, 'w')
+    saved_translations_file.write(json.dumps(saved_translations))  # Write the translations
+    saved_translations_file.close()
+    print("Saved!")
+
 
 class WatsonTranslator:
     def __init__(self, version, api_key):
@@ -39,6 +70,9 @@ class WatsonTranslator:
             version='2018-05-01',
             iam_apikey=self.api_key,
             url='https://gateway' + url_location + '.watsonplatform.net/language-translator/api')
+
+        if saved_translations == {'detections': {}, 'translations': {}}:
+            load_translations()
 
     def auto_translate(self, input_text, output_language='en', pick_language=0):
         """
@@ -67,6 +101,10 @@ class WatsonTranslator:
     def detect_language(self, input_text, pick_language=0):
         print("Detecting", input_text)
 
+        if input_text in saved_translations['detections']:
+            print("Using previously detected")
+            return saved_translations['detections'][input_text]
+
         input_language = self.language_translator.identify(input_text).get_result()
         confidence = input_language["languages"][pick_language]['confidence']
         input_language = input_language["languages"][pick_language]["language"]
@@ -74,8 +112,10 @@ class WatsonTranslator:
         print("Result:", input_language, confidence)
 
         if confidence > 0.3:
+            saved_translations['detections'][input_text] = input_language
             return input_language
         else:
+            saved_translations['detections'][input_text] = 'en'
             return 'en'
 
     def translate_text(self, input_text, input_language, output_language, emoji_names=[]):
@@ -83,6 +123,12 @@ class WatsonTranslator:
             return input_text
 
         print("Translating", input_text, input_language, output_language)
+
+        if input_language in saved_translations['translations']:
+            if output_language in saved_translations['translations'][input_language]:
+                if input_text in saved_translations['translations'][input_language][output_language]:
+                    print("Using saved translation")
+                    return saved_translations['translations'][input_language][output_language][input_text]
 
 
         input_list = re.split('(:|_|\*|~~)', input_text)
@@ -95,7 +141,6 @@ class WatsonTranslator:
         print(input_list)
         print(output_list['translations'])
         print('==============================')
-
 
         output_text = ""
 
@@ -123,5 +168,22 @@ class WatsonTranslator:
 
         print("Result:", output_text)
 
+        if input_language not in    saved_translations['translations']:
+            saved_translations['translations'][input_language] = {}
+
+        if output_language not in   saved_translations['translations'][input_language]:
+            saved_translations['translations'][input_language][output_language] = {}
+
+        if input_text not in        saved_translations['translations'][input_language][output_language]:
+            saved_translations['translations'][input_language][output_language][input_text] = output_text
+
         return output_text
 
+
+if __name__ == "__main__":
+    translator_api_path = os.path.normpath('ID/TRANSLATOR_API_KEY.txt')
+    TRANSLATOR_API_KEY = str.rstrip(open(translator_api_path).read())
+    wt = WatsonTranslator('2019-05-21', TRANSLATOR_API_KEY)
+    print('Test Result:', wt.auto_translate("_Hola, soy Niko! ¿Cómo estás?_ :niko_speak:"))
+
+save_translations()
